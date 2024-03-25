6234c3e93baa97d3b909b2ec176c21600d58fca2
Shivansh Shukla <shivanshnshukla@gmail.com>
2024-03-25 14:14:29 -0700

    updated the budget file
    updated the create chart function

--- Budget App/budget_app.py
+++ Budget App/budget_app.py
@@ -10,13 +10,12 @@ class Category:
             display = display + f"{item['description'][:23]:<23}{item['amount']:>7.2f}\n"
         
         total = self.get_balance()
-        display = display + f"Total: {total:.2f}\n"
+        display = display + f"Total: {total:.2f}"
         
         return display
 
     def deposit(self, amount, description=""):
         self.ledger.append({'amount':amount, 'description':description})
-        #return self.ledger
         
     def withdraw(self, amount, description=""):
         if not self.check_funds(amount):
@@ -27,12 +26,12 @@ class Category:
         
     def get_balance(self):
         
-        self.first_dep = self.ledger[0]['amount']
+        balance = 0
         
-        for dep_with in self.ledger[1:]:
-            self.first_dep = self.first_dep + dep_with['amount']
+        for dep_with in self.ledger:
+            balance += dep_with['amount']
             
-        return self.first_dep    
+        return balance    
         
     
     def transfer(self, amount, budg_category):
@@ -47,69 +46,54 @@ class Category:
             return True
         return False
 
-def create_spend_chart(*categories):
+def create_spend_chart(categories):
     
     start = "Percentage spent by category"
     
-    categ_spent = {}
-    #categ_deposit = {}
+    categ_spent = []
     
     for  categ in categories:
         spent_money = 0
-        #tot_deposit_money = 0
         
         for item in categ.ledger:
             if item['amount'] < 0:
                 spent_money += abs(item['amount'])
-            continue
-            # else:
-            #     tot_deposit_money += item['amount']
         
-        categ_spent[categ.name] = spent_money
-        #categ_deposit[categ.name] = tot_deposit_money
+        categ_spent.append(spent_money) 
     
-    #print(categ_spent)
-    # print(categ_spent.values())
-    #print(categ_deposit)
+    total_spent_all = sum(categ_spent)
+    spent_percent = [(i/total_spent_all) * 100 for i in categ_spent]
+    #rounded_percent = [(val//10) * 10 for val in spent_percent]
     
-    # print(list(categ_spent.values()))
-    
-    total_spent_all = sum(list(categ_spent.values()))
-    # print(total_spent_all) 
-    
-    spent_percent = {key: int((val/total_spent_all) * 100) for key, val in categ_spent.items()}
-    rounded_percent = {key: val//10 * 10 for key,val in spent_percent.items()}
-    
-    for i in range(100, -10, -10):
+    for i in range(100, -1, -10):
         start += f"\n{i:>3}|"
-        #print(f"{i:>3}|")
 
-        for val in rounded_percent.values():
-            if i <= val:
+        for val in spent_percent:
+            if val > i:
                 start += ' o '
             else:
                 start += '   '
         
-    start += f"\n    {'-' * (len(rounded_percent) * 3 + 1)}"        
+        start += ' '
+        
+    start += "\n    ----------" #f"\n    {'-' * (len(rounded_percent) * 3 + 1)}"        
     
-    #print(max(list(rounded_percent.keys()), key = len))
+    categ_name_length = []
+    for categ in categories:
+        categ_name_length.append(len(categ.name))
     
-    max_row_len = len(max(list(rounded_percent.keys()), key = len))
-    # print(max_row_len)
+    max_row_len = max(categ_name_length)
     
     for row_sec_half in range(max_row_len):
         start += "\n    "
         
-        for i in range(len(rounded_percent)):
-            if row_sec_half < len(list(rounded_percent.keys())[i]):
-                start += f" {list(rounded_percent.keys())[i][row_sec_half]} "
+        for i in range(len(spent_percent)):
+            if row_sec_half < categ_name_length[i]:
+                start += f" {categories[i].name[row_sec_half]} "
             else:
                 start += "   "
         
-    start += '\n'   
-        
-    # print(spent_percent)
-    # print(rounded_percent)
+        start += ' '
     
     return start
     
@@ -127,10 +111,10 @@ food.transfer(50, clothing)
 clothing.deposit(300, 'Paycheck came in')
 clothing.withdraw(65, 'Nike Sweats')
 clothing.transfer(30, food)
-# auto = Category("Auto")
-# auto.deposit(100, "deposit")
-# clothing.transfer(50, auto)
-# auto.withdraw(70, 'car spray')
+auto = Category("Auto")
+auto.deposit(100, "deposit")
+clothing.transfer(50, auto)
+auto.withdraw(70, 'car spray')
 
 
 
@@ -138,4 +122,4 @@ print(food)
 print(clothing)
 # print(auto)
 
-print(create_spend_chart(food, clothing)) #, auto
+print(create_spend_chart((food, clothing, auto))) #
